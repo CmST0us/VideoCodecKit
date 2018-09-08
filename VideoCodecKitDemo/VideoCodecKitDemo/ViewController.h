960ca115be9b9703dcc8e343cf3c37331fabc44b
CmST0us <eric3u@outlook.com>
2018-09-08 23:56:43 +0800

    [Feat] 使用Objective-C工程 [Feat] 重新创建工程，使用VideoCodecKit工程名

---
+++ VideoCodecKitDemo/VideoCodecKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoCodecKitDemo
+//
+//  Created by CmST0us on 2018/9/8.
+//  Copyright © 2018年 eric3u. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VideoCodecKitDemo/VideoCodecKitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VideoCodecKitDemo
+//
+//  Created by CmST0us on 2018/9/8.
+//  Copyright © 2018年 eric3u. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
