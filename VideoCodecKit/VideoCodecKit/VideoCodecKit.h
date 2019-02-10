caf676666e9a797be54b7c33a07fd59290e23731
eric3u <eric3u@outlook.com>
2019-02-10 14:37:38 +0800

    [Fix] 头文件添加平台判断

--- VideoCodecKit/VideoCodecKit/VideoCodecKit.h
+++ VideoCodecKit/VideoCodecKit/VideoCodecKit.h
@@ -5,7 +5,7 @@
 //  Created by CmST0us on 2018/10/1.
 //  Copyright © 2018年 eric3u. All rights reserved.
 //
-
+#if TARGET_OS_IOS
 #import <UIKit/UIKit.h>
 
 //! Project version number for VideoCodecKit.
@@ -13,6 +13,7 @@ FOUNDATION_EXPORT double VideoCodecKitVersionNumber;
 
 //! Project version string for VideoCodecKit.
 FOUNDATION_EXPORT const unsigned char VideoCodecKitVersionString[];
+#endif
 
 #import <VideoCodecKit/VCByteArray.h>
 #import <VideoCodecKit/VCSafeObjectQueue.h>
